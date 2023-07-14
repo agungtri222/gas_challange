d4da9259db044420d5e3833c49e3468a9caaf81b
Agung Trilaksono <agungtrilaksono123@gmail.com>
2023-07-14 09:43:53 +0700

    initial commit

--- contracts/gasChallenge.sol
+++ contracts/gasChallenge.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 contract gasChallenge {
     //Implement Fixed-Size Array Technique Here
-    uint[] numbers = [1,2,3,4,5,6,7,8,9,10];
+    uint[10] numbers;
     
     //Function to check for sum of array
     //No changes required in this function
@@ -24,6 +24,15 @@ contract gasChallenge {
     //Implement Remaining Gas Optimization Techniques Here
     //Sum of elements in the numbers array should equal 0
     function optimizedFunction() public {
-      
+        uint sum = 0;
+        uint length = numbers.length;
+
+        unchecked {
+            for (uint i = 0; i < length; i++) {
+                sum += numbers[i];
+            }
+        }
+
+        sum == 0;
     }
- }
+}
